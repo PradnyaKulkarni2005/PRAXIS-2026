fb7237b8bc6189c18c9b3a414bab714e5902ed32
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-01-16 19:21:57 +0530

    Fix model-viewer by bundling locally for production

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import { AudioProvider } from './context/AudioContext'
+import '@google/model-viewer';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
