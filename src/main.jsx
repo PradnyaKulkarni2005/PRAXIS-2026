5277a47fcb9fc508e277d48d1c43b7492461c14b
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-01-28 18:18:19 +0530

    added analytics component to track visitors

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import { AudioProvider } from './context/AudioContext'
 import '@google/model-viewer';
-
+import { Analytics } from '@vercel/analytics/react'; // 1. Import the component
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AudioProvider>
         <App />
+        <Analytics /> {/* 2. Add the component here */}
       </AudioProvider>
     </BrowserRouter>
   </StrictMode>
